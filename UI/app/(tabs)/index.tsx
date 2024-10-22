c3304f02649854ddf3a0384707ea637ab4043637
Manidhar <manidharreddy767@gmail.com>
2024-10-21 23:24:17 -0700

    Added Login/Signup page code in UI folder

---
+++ UI/app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import AuthScreen from '@/components/AuthScreen';
+import { View } from 'react-native';
+
+export default function Page() {
+  return (
+    <View style={{ flex: 1 }}>
+      <AuthScreen />
+    </View>
+  );
+}
