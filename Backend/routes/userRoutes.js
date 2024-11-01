497435719bd20f75ca4210ea345e4159fee9f435
Phuoc Le <phuocle3210@gmail.com>
2024-11-01 11:51:36 -0700

    Implemented basic follower/following backend

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const User = require("../data_schemas/users");
 const router = express.Router();
 
+// -----------------------------------------
+// Basic User CRUD
+// -----------------------------------------
+
 // Create a new user
 router.post("/", async (req, res) => {
   try {
@@ -42,8 +46,7 @@ router.put("/:id", async (req, res) => {
       new: true,
       runValidators: true,
     });
-    if (!updatedUser)
-      return res.status(404).json({ error: "User not found" });
+    if (!updatedUser) return res.status(404).json({ error: "User not found" });
     res.json(updatedUser);
   } catch (error) {
     res.status(400).json({ error: "Error updating user by ID" });
@@ -54,12 +57,62 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   try {
     const deletedUser = await User.findByIdAndDelete(req.params.id);
-    if (!deletedUser)
-      return res.status(404).json({ error: "User not found" });
+    if (!deletedUser) return res.status(404).json({ error: "User not found" });
     res.json({ message: "User deleted successfully" });
   } catch (error) {
     res.status(500).json({ error: "Error deleting user by ID" });
   }
 });
+
+// -----------------------------------------
+// Followers and Followings
+// -----------------------------------------
+
+// Add a follower to a user by ID
+router.post("/:id/followers", async (req, res) => {
+  try {
+    // add follower to user
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ error: "User not found" });
+    user.followers.push(req.body.follower_id);
+    await user.save();
+
+    // add user to follower's following list
+    const follower = await User.findById(req.body.follower_id);
+    if (!follower) return res.status(404).json({ error: "Follower not found" });
+    follower.following.push(req.params.id);
+    await follower.save();
+
+    res.json({user, follower});
+  } catch (error) {
+    res.status(400).json({ error: "Error adding follower to user" });
+  }
+});
+
+// Remove a follower from a user by ID
+router.delete("/:id/followers", async (req, res) => {
+  try {
+    // remove follower from user
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ error: "User not found" });
+    user.followers = user.followers.filter(
+      (follower) => follower != req.body.follower_id
+    );
+    await user.save();
+
+    // remove user from follower's following list
+    const follower = await User.findById(req.body.follower_id);
+    if (!follower) return res.status(404).json({ error: "Follower not found" });
+    follower.following = follower.following.filter(
+      (following) => following != req.params.id
+    );
+    await follower.save();
+
+    res.json({user, follower});
+  } catch (error) {
+    res.status(400).json({ error: "Error removing follower from user" });
+  }
+});
+
 
 module.exports = router;
