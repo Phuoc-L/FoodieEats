a64f02e0e7c0b9cb1e28539d5d837acf3e0f66fa
Sarah Lau <32653290+slau-cmpe@users.noreply.github.com>
2025-02-09 21:30:56 -0800

    Template for Stack Navigation
    Here is the start of a stack navigation to navigate between screens. It includes a dummy profile screen for now. The stack navigation comes with a header for back navigate to the previous screen.

--- FoodieEats/App.js
+++ FoodieEats/App.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import AuthScreen from './screens/AuthScreen';
+import ProfileScreen from './screens/ProfileScreen'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
-  return <AuthScreen />;
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Auth'>
+        <Stack.Screen name='Auth' component={AuthScreen}/>
+        <Stack.Screen name='Profile' component={ProfileScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
