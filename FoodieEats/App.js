ce7d1e5e4f9a1541d2816bfb5630ea25aa875ae6
Sarah Lau <32653290+slau-cmpe@users.noreply.github.com>
2025-02-18 08:17:42 -0800

    Update App.js
    Fix merge conflict

--- FoodieEats/App.js
+++ FoodieEats/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import AuthScreen from './screens/AuthScreen';
-import ProfileScreen from './screens/ProfileScreen'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AuthScreen from './screens/AuthScreen';
+import UserFeed from './screens/UserFeed';
+import Explore from './screens/Explore';
+import Profile from './screens/Profile';
+import NewPost from './screens/NewPost';
+
+//export default function App() {
+//  return <AuthScreen />;
+//}
+
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const Stack = createNativeStackNavigator();
-
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Auth'>
-        <Stack.Screen name='Auth' component={AuthScreen}/>
-        <Stack.Screen name='Profile' component={ProfileScreen}/>
+      <Stack.Navigator initialRouteName="UserFeed">
+        <Stack.Screen name="UserFeed" component={UserFeed} options={{ headerShown: false }} />
+        <Stack.Screen name="Explore" component={Explore} options={{ headerShown: false }} />
+        <Stack.Screen name="Profile" component={Profile} options={{ headerShown: false }} />
+        <Stack.Screen name="NewPost" component={NewPost} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
