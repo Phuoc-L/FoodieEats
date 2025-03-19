89e26ada93d136866e390a11efc952f4495a5f12
Miranda <mlivengd@gmail.com>
2025-03-19 13:20:03 -0700

    Resolving conflicts in /FoodieEats/App.js for merge

--- FoodieEats/App.js
+++ FoodieEats/App.js
@@ -6,23 +6,18 @@ import UserFeed from './screens/UserFeed';
 import Explore from './screens/Explore';
 import Profile from './screens/Profile';
 import NewPost from './screens/NewPost';
-import RestaurantPage from './screens/RestaurantPage';
-import DishReviews from './screens/DishReviews';
-
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="RestaurantPage">
+      <Stack.Navigator initialRouteName="Auth">
         <Stack.Screen name="Auth" component={AuthScreen} options={{ headerShown: false }} />
         <Stack.Screen name="UserFeed" component={UserFeed} options={{ headerShown: false }} />
         <Stack.Screen name="Explore" component={Explore} options={{ headerShown: false }} />
-        <Stack.Screen name="Profile" component={Profile} options={{ headerShown: false }} />
+        <Stack.Screen name="Profile" component={Profile} options={{ headerShown: true }} />
         <Stack.Screen name="NewPost" component={NewPost} />
-        <Stack.Screen name="RestaurantPage" component={RestaurantPage} options={{ headerShown: true }} />
-        <Stack.Screen name="DishReviews" component={DishReviews} options={{ title: "Dish Reviews" }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
